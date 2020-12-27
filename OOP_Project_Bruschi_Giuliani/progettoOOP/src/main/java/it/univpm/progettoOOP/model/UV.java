a57186997110dc22a87e5836097d8e143596220a
BruschiSimone <s1089299@studenti.univpm.it>
2020-12-27 22:21:10 +0100

    class UV

---
+++ OOP_Project_Bruschi_Giuliani/progettoOOP/src/main/java/it/univpm/progettoOOP/model/UV.java
@@ -0,0 +1,16 @@
+package it.univpm.progettoOOP.model;
+
+public class UV {
+	
+	private double value;
+	
+	public UV(double value) {
+		this.value = value;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+//	public void setValue(double value) {this.value = value;}
+}

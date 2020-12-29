8e6bf98e562fc3a53324ca091e7efc9061643844
BruschiSimone <s1089299@studenti.univpm.it>
2020-12-29 12:01:00 +0100

    city class

--- OOP_Project_Bruschi_Giuliani/progettoOOP/src/main/java/it/univpm/progettoOOP/model/City.java
+++ OOP_Project_Bruschi_Giuliani/progettoOOP/src/main/java/it/univpm/progettoOOP/model/City.java
@@ -1,5 +1,28 @@
 package it.univpm.progettoOOP.model;
 
 public class City {
+		
+		private String name;
+		private String country;
 
+		public City(String name, String country) {
+			this.name = name;
+			this.country= country;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getCountry() {
+			return country;
+		}
+
+		public void setCountry(String country) {
+			this.country = country;
+		}
 }

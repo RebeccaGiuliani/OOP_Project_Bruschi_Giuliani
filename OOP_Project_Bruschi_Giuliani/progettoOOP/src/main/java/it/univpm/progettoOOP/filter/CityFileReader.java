e7b63598767139dae6b2da46c8823656932e4bbb
BruschiSimone <s1089299@studenti.univpm.it>
2021-01-02 21:19:24 +0100

    Filtro file città

--- OOP_Project_Bruschi_Giuliani/progettoOOP/src/main/java/it/univpm/progettoOOP/filter/CityFileReader.java
+++ OOP_Project_Bruschi_Giuliani/progettoOOP/src/main/java/it/univpm/progettoOOP/filter/CityFileReader.java
@@ -1,57 +1,72 @@
 package it.univpm.progettoOOP.filter;
 
 import it.univpm.progettoOOP.model.City;
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Scanner;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.PrintWriter;
 import java.io.IOException;
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class CityFileReader implements CityFileReaderService {
 
 	private static String name = "city.list.json";	
 	private double lat, lon;
-	private City city;
+	private String nameCity, country;
 
 	public CityFileReader(City city) {
-		this.city = city;
+		this.nameCity = city.getName();
+		this.country = city.getCountry();
+		
+		JSONArray ja = new JSONArray();
+		ja = caricaArray();
+		
+		getCity(ja, nameCity, country);
+		System.out.println(lat);
+		System.out.println(lon);
 	}
 
-	public void readFile () {
-		try {
-			Scanner file_input = new Scanner(new BufferedReader (new FileReader(name)));
-			System.out.println(name);
-			file_input.close();
-		} 
-		catch (FileNotFoundException e) {
+	public JSONArray caricaArray() {
+		JSONParser jsonParser = new JSONParser();
+
+		try (FileReader reader = new FileReader(name)){
+			//Read JSON file
+			Object obj = jsonParser.parse(reader);
+			JSONArray cityList = new JSONArray();
+			return cityList = (JSONArray) obj;
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+		return null;
 	}
-	
 
-/*	public void stampaFile () {
-		try {
-			PrintWriter file_output = new PrintWriter(new BufferedWriter (new FileWriter(name)));
-			
-			
-			file_output.close();
-		} 
-		catch (IOException e) {
-			System.out.println("ERORE di I/O");
-			System.out.println(e);
+	public void getCity(JSONArray ja, String name, String country) {
+		//Get city object within list
+		for(int i =0; i<ja.size(); i++) {
+			JSONObject cityObject = (JSONObject) ja.get(i);
+			if(cityObject.get("name").equals(name)) {
+				if(cityObject.get("country").equals(country)) {
+					//Get coord object within list
+					JSONObject coordObject = (JSONObject) cityObject.get("coord");
+					//Get lon
+					this.lon = (Double) coordObject.get("lon");    
+					//Get lat
+					this.lat = (Double) coordObject.get("lat");  
+				}
+			}
 		}
-	}*/
-
-	public double getLat() {
-
-		return lat;
 	}
-
+	public double getLat() {
+		return this.lat;
+	}
 	public double getLon() {
-		return lon;
+		return this.lon;
 	}
 }
--- OOP_Project_Bruschi_Giuliani/progettoOOP/src/main/java/it/univpm/progettoOOP/filter/CityFileReaderService.java
+++ OOP_Project_Bruschi_Giuliani/progettoOOP/src/main/java/it/univpm/progettoOOP/filter/CityFileReaderService.java
@@ -1,6 +1,10 @@
 package it.univpm.progettoOOP.filter;
 
+import org.json.simple.JSONArray;
+
 public interface CityFileReaderService {
+	public abstract void getCity(JSONArray ja, String nameCity, String country);
+	public abstract JSONArray caricaArray();
 	public abstract double getLat();
 	public abstract double getLon();
 }

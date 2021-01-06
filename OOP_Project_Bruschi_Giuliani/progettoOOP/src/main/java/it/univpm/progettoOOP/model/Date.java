905ddc7ec98b2f290f8000936e1ddf73a334ffec
BruschiSimone <s1089299@studenti.univpm.it>
2021-01-06 21:03:53 +0100

    date

--- OOP_Project_Bruschi_Giuliani/progettoOOP/src/main/java/it/univpm/progettoOOP/model/Date.java
+++ OOP_Project_Bruschi_Giuliani/progettoOOP/src/main/java/it/univpm/progettoOOP/model/Date.java
@@ -10,33 +10,27 @@ public class Date {
 		
 		String[] param0 = date.split("T");
 		String date0 = param0[0];
-		//String date1 = param0[1];
 		
 		this.date = date0;
 		String[] param = date0.split("-");
 		this.year = param[0];
 		this.month = param[1];
 		this.day = param[2];
-		//System.out.println("data:"+this.date);
 	}
 
 	public String getDay(){
-		//System.out.println("getDay: "+this.day);
 		return this.day;
 	} 
 
 	public String getMonth(){
-		//System.out.println("getMonth: "+this.month);
 		return this.month;
 	}
 
 	public String getYear(){
-		//System.out.println("getYear: "+this.year);
 		return this.year;
 	}
 	
 	public String getDate() {
-		//System.out.println("getDate: "+this.date);
 		return this.date;
 	}
 }

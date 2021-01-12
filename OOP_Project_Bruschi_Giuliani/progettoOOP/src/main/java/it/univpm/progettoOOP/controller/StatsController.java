8b4e0aa32331d97acd36520f2910be74140f9236
RebeccaGiuliani <s.1087891@studenti.univpm.it>
2021-01-12 17:10:04 +0100

    Updates StatsController

--- OOP_Project_Bruschi_Giuliani/progettoOOP/src/main/java/it/univpm/progettoOOP/controller/StatsController.java
+++ OOP_Project_Bruschi_Giuliani/progettoOOP/src/main/java/it/univpm/progettoOOP/controller/StatsController.java
@@ -1,5 +1,30 @@
 package it.univpm.progettoOOP.controller;
 
-public class StatsController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import it.univpm.progettoOOP.filter.APICall;
+import it.univpm.progettoOOP.stats.Stats;
+
+@RestController
+public class StatsController {
+	
+	APICallController c = new APICallController();
+	
+	APICall call = c.chiamata_api(1, 1, 2019, 10, 1, 2019, "Ancona", "IT");
+	
+	@RequestMapping(value = "/data/stats/max&min", method = RequestMethod.GET) 
+	public String stats2() {
+		Stats s = new Stats(call);
+		return "Il valore massimo è "+s.getMax()+", il valore minimo è "+ s.getMin();
+	}
+	
+	@RequestMapping(value = "/data/stats", method = RequestMethod.GET) 
+	public Stats stats() {
+		APICallController c = new APICallController();
+		APICall call = c.chiamata_api(1,1,2019,10,1,2019, "Ancona", "IT");
+		
+		return new Stats(call);
+	}
 }

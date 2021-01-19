ac143f1531682b1b1055ebe28b0396fc501d2212
RebeccaGiuliani <s.1087891@studenti.univpm.it>
2021-01-19 11:26:31 +0100

    test eccezioni

--- OOP_Project_Bruschi_Giuliani/progettoOOP/src/test/java/it/univpm/progettoOOP/test/WrongCityExceptionTest.java
+++ OOP_Project_Bruschi_Giuliani/progettoOOP/src/test/java/it/univpm/progettoOOP/test/WrongCityExceptionTest.java
@@ -1,14 +1,110 @@
 package it.univpm.progettoOOP.test;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.json.simple.JSONArray;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class WrongCityExceptionTest {
+import it.univpm.progettoOOP.exception.WrongCityException;
+import it.univpm.progettoOOP.filter.CityFileReader;
+import it.univpm.progettoOOP.model.City;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+/**
+ *<p>
+ *L'unità di test <b>WrongCityExceptionTest</b> è generata per testare la classe <b>WrongCityException</b>.
+ *</p>
+ *
+ * @author RebeccaGiuliani
+ * @author SimoneBruschi
+ *
+ */
+
+class WrongCityExceptionTest {
+	
+	WrongCityException e;
+	CityFileReader c1,c2,c3;
+	JSONArray ja;
+	
+	/**
+	 * 
+	 * @throws Exception eccezione
+	 */
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		c1 = new CityFileReader(new City("Ancon", "IT"));
+		c2 = new CityFileReader(new City("Ancona", "I"));
+		c3 = new CityFileReader(new City("Roma", "IT"));
+		ja = new JSONArray();
+	}
+	
+	/**
+	 * 
+	 * @throws Exception eccezione
+	 */
+	
+	@AfterEach
+	void tearDown() throws Exception {
 	}
 
+	/**
+	 * test per la generazione dell'eccezione <b>WrongCityException<b> 
+	 * nel caso di inserimento del nome della città in modo errato
+	 * 
+	 */
+	
+	@Test
+	@DisplayName("Eccezione Città Sbagliata Funzionante")
+	void WrongCityExceptionTest1() {
+		
+		ja = c1.caricaArray();
+		
+		e = assertThrows(WrongCityException.class, () -> {
+			c1.getCity(ja);;
+		}) ;
+		
+		assertEquals("ERR: La città o lo stato inseriti non sono validi!", e.getMex());
+	}
+	
+	/**
+	 * test per la generazione dell'eccezione <b>WrongCityException<b> 
+	 * nel caso di inserimento della sigla dello stato in modo errato
+	 * 
+	 */
+	
+	@Test
+	@DisplayName("Eccezione Città Sbagliata Funzionante")
+	void WrongCityExceptionTest2() {
+		
+		ja = c2.caricaArray();
+		
+		e = assertThrows(WrongCityException.class, () -> {
+			c2.getCity(ja);
+		}) ;
+		
+		assertEquals("ERR: La città o lo stato inseriti non sono validi!", e.getMex());
+	}
+	
+	/**
+	 * test per la generazione dell'eccezione <b>WrongCityException<b> 
+	 * nel caso di inserimento del nome della città in italiano, in quanto nel file le città sono salvate in inglese
+	 * 
+	 */
+
+	@Test
+	@DisplayName("Eccezione Città Sbagliata Funzionante")
+	void WrongCityExceptionTest3() {
+		
+		ja = c3.caricaArray();
+		
+		e = assertThrows(WrongCityException.class, () -> {
+			c3.getCity(ja);
+		}) ;
+		
+		assertEquals("ERR: La città o lo stato inseriti non sono validi!", e.getMex());
+	}
 }
--- OOP_Project_Bruschi_Giuliani/progettoOOP/src/test/java/it/univpm/progettoOOP/test/WrongPeriodExceptionTest.java
+++ OOP_Project_Bruschi_Giuliani/progettoOOP/src/test/java/it/univpm/progettoOOP/test/WrongPeriodExceptionTest.java
@@ -1,14 +1,110 @@
 package it.univpm.progettoOOP.test;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import it.univpm.progettoOOP.exception.WrongPeriodException;
+import it.univpm.progettoOOP.model.Period;
+
+/**
+ *<p>
+ *L'unità di test <b>WrongPeriodExceptionTest</b> è generata per testare la classe <b>WrongPeriodException</b>.
+ *</p>
+ *
+ * @author RebeccaGiuliani
+ * @author SimoneBruschi
+ *
+ */
+
 class WrongPeriodExceptionTest {
+	
+	WrongPeriodException e;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	/**
+	 * 
+	 * @throws Exception eccezione
+	 */
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
 	}
-
+	
+	/**
+	 * 
+	 * @throws Exception eccezione
+	 */
+	
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+	/**
+	 * test per la generazione dell'eccezione <b>WrongPeriodException<b> 
+	 * nel caso di inserimento di un periodo che termina prima del 22.6.2017
+	 * 
+	 */
+	
+	@Test
+	@DisplayName("Eccezione Periodo Sbagliato Funzionante")
+	void WrongPeriodExceptionTest1() {
+		e = assertThrows(WrongPeriodException.class, () -> {
+			new Period(1,1,2017,31,1,2017);
+		}) ;
+		
+		assertEquals("ERR: Il periodo inserito non è valido!", e.getMex());
+	}
+	
+	/**
+	 * test per la generazione dell'eccezione <b>WrongPeriodException<b> 
+	 * nel caso di inserimento di un valore errato per il giorno di fine
+	 * 
+	 */
+	
+	@Test
+	@DisplayName("Eccezione Periodo Sbagliato Funzionante")
+	void WrongPeriodExceptionTest2() {
+		e = assertThrows(WrongPeriodException.class, () -> {
+			new Period(1,1,2019,32,1,2019);
+		}) ;
+		
+		assertEquals("ERR: Il periodo inserito non è valido!", e.getMex());
+	}
+	
+	/**
+	 * test per la generazione dell'eccezione <b>WrongPeriodException<b> 
+	 * nel caso di inserimento di un valore errato per il mese di fine
+	 * 
+	 */
+	
+	@Test
+	@DisplayName("Eccezione Periodo Sbagliato Funzionante")
+	void WrongPeriodExceptionTest3() {
+		e = assertThrows(WrongPeriodException.class, () -> {
+			new Period(1,1,2019,31,13,2019);
+		}) ;
+		
+		assertEquals("ERR: Il periodo inserito non è valido!", e.getMex());
+	}
+	
+	/**
+	 * test per la generazione dell'eccezione <b>WrongPeriodException<b> 
+	 * nel caso di inserimento di un valore errato per il giorno di inizio
+	 * 
+	 */
+	
+	@Test
+	@DisplayName("Eccezione Periodo Sbagliato Funzionante")
+	void WrongPeriodExceptionTest4() {
+		e = assertThrows(WrongPeriodException.class, () -> {
+			new Period(0,1,2019,31,12,2019);
+		}) ;
+		
+		assertEquals("ERR: Il periodo inserito non è valido!", e.getMex());
+	}
 }

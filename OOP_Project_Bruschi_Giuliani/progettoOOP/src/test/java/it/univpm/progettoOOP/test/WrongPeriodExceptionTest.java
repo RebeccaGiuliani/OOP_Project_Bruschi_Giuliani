3f5c354a19004df81fe7b64a5c4426e2867f4533
BruschiSimone <s1089299@studenti.univpm.it>
2021-01-19 10:23:56 +0100

    test class

---
+++ OOP_Project_Bruschi_Giuliani/progettoOOP/src/test/java/it/univpm/progettoOOP/test/WrongPeriodExceptionTest.java
@@ -0,0 +1,14 @@
+package it.univpm.progettoOOP.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WrongPeriodExceptionTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
